1c3ce511a5ac19676054d4ee114885668f5de1a1
legkvla <vladimir.legkunets@gmail.com>
2018-04-07 17:12:17 +0300

    Adding repos config

--- src/recomotion_backend/storages/config.clj
+++ src/recomotion_backend/storages/config.clj
@@ -8,8 +8,9 @@
 (defn wrap-storages [handler mongo-db]
   (fn [& args]
     (binding [
-      events-storage (init-events mongo-db)
       content-storage (init-content mongo-db)
+      events-storage (init-events mongo-db)
+      impressions-storage (init-impressions mongo-db)
       ]
       (apply handler args)
       )
